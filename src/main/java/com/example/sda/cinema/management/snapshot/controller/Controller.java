0a7ae4b58e21a8da03b0769a3b99d870df8941e5
Lukasz <lukww@wp.pl>
2018-06-10 15:30:41 +0200

    ticket endpoint

--- src/main/java/com/example/sda/cinema/management/snapshot/controller/Controller.java
+++ src/main/java/com/example/sda/cinema/management/snapshot/controller/Controller.java
@@ -4,9 +4,11 @@ package com.example.sda.cinema.management.snapshot.controller;
 import com.example.sda.cinema.management.snapshot.model.Movie;
 import com.example.sda.cinema.management.snapshot.model.MovieCategory;
 import com.example.sda.cinema.management.snapshot.service.CategoryService;
+import com.example.sda.cinema.management.snapshot.model.Ticket;
 import com.example.sda.cinema.management.snapshot.service.MovieService;
 import com.example.sda.cinema.management.snapshot.model.Seance;
 import com.example.sda.cinema.management.snapshot.service.SeanceService;
+import com.example.sda.cinema.management.snapshot.service.TicketService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +28,9 @@ public class Controller {
 
     @Autowired
     private CategoryService categoryService;
+    @Autowired
+    private TicketService ticketService;
+
 
     @RequestMapping(value = "/addmovie", method = RequestMethod.POST)
     public Movie addMovie (@RequestBody Movie movie) {return movieService.save(movie);}
@@ -43,10 +48,22 @@ public class Controller {
     public Seance addSeance(@RequestBody Seance seance){
         return seanceService.save(seance);
     }
+
     @RequestMapping(value = "/getseance", method = RequestMethod.GET)
     public List<Seance> seanceList(){
         return seanceService.getSeance();
     }
+
+    @RequestMapping(value="/addticket", method =RequestMethod.POST)
+    public Ticket addTicket(@RequestBody Ticket ticket){
+        return ticketService.save(ticket);
+    }
+
+    @RequestMapping(value = "/getticket", method = RequestMethod.GET)
+    public List<Ticket> ticketList(){
+        return ticketService.getTicket();
+    }
+
 
 
 }
--- src/main/java/com/example/sda/cinema/management/snapshot/service/TicketService.java
+++ src/main/java/com/example/sda/cinema/management/snapshot/service/TicketService.java
@@ -1,4 +1,23 @@
 package com.example.sda.cinema.management.snapshot.service;
 
+import com.example.sda.cinema.management.snapshot.model.Ticket;
+import com.example.sda.cinema.management.snapshot.repository.TicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TicketService {
+
+    @Autowired
+    private TicketRepository ticketRepository;
+
+    public List<Ticket> getTicket(){
+        return ticketRepository.findAll();
+    }
+
+    public Ticket save(Ticket ticket) {
+        return ticketRepository.save(ticket);
+    }
 }
